26b09681b8bda0ad5c1ef034ffb3668fc3267112
Anheru88 <ajimenez@digitalcaribe.com.co>
2015-05-05 10:16:52 -0500

    New form, used to search student by name

--- AdapterPatern/Forms/StudentForm.cs
+++ AdapterPatern/Forms/StudentForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdapterPatern.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,42 @@ namespace AdapterPatern.Forms
         public StudentForm()
         {
             InitializeComponent();
+        }
+
+        private void tAge_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Para obligar a que sólo se introduzcan números 
+            if (Char.IsDigit(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+                if (Char.IsControl(e.KeyChar)) //permitir teclas de control como retroceso 
+                {
+                    e.Handled = false;
+                }
+                else
+                {
+                    //el resto de teclas pulsadas se desactivan 
+                    e.Handled = true;
+                }
+
+        }
+
+        private void bSave_Click(object sender, EventArgs e)
+        {
+            Student s = new Student();
+            s.name = tName.Text;
+            s.code = tCode.Text;
+            s.age = Convert.ToInt32(tAge.Text);
+            s.gender = "";
+            s.address = tAddress.Text;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
