1530f5b489f25a405b741554571ab02d0909a720
Anheru88 <ajimenez@digitalcaribe.com.co>
2015-05-05 11:42:58 -0500

    Create a class to use dictionary for save the data

---
+++ AdapterPatern/Adapters/DictionarySaveAdapter.cs
@@ -0,0 +1,29 @@
+﻿using AdapterPatern.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPatern.Adapters
+{
+    class DictionarySaveAdapter : SaveAdapter
+    {
+
+        private Dictionary<String, Student> StudentDictionary;
+
+        public void setElement(Student s)
+        {
+            StudentDictionary.Add(s.name, s);
+        }
+
+        public Student getElement(String name)
+        {
+            if (StudentDictionary.ContainsKey(name))
+            {
+                return StudentDictionary[name];
+            }
+            else { return null; }
+        }
+    }
+}
