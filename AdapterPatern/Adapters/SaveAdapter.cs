e058246083324711632fa40e4e8922d5eb19e8c7
Anheru88 <ajimenez@digitalcaribe.com.co>
2015-05-05 11:41:45 -0500

    Create the interface for the adapters

---
+++ AdapterPatern/Adapters/SaveAdapter.cs
@@ -0,0 +1,15 @@
+﻿using AdapterPatern.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPatern.Adapters
+{
+    interface SaveAdapter
+    {
+        public void setElement(Student s);
+        public Student getElement(String name);
+    }
+}
