b978784884e3c28dd9ee0c8ef7af16097d247f6c
Anheru88 <ajimenez@digitalcaribe.com.co>
2015-05-05 00:54:10 -0500

    Add Student Class

---
+++ AdapterPatern/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPatern.Models
+{
+    class Student
+    {
+        public String name { get; set; }
+        public String code { get; set; }
+        public int age { get; set; }
+        public String gender { get; set; }
+        public String address { get; set; }
+    }
+}
